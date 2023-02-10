590f9fd2d382833ce76e687c5d9f2300dda01c61
valmorflores <valmorflores@gmail.com>
2023-02-10 12:41:32 -0300

    update for display table format

--- app/view/view_user_data_content.php
+++ app/view/view_user_data_content.php
@@ -1,20 +1,26 @@
- <table border='1'>
-   <tr>
-      <th>ID</th>    
-      <th>Nome</th>    
-      <th>Email</th>
-      <th>Ação</th>    
-   </tr>
+<table class="mdl-data-table mdl-js-data-table mdl-shadow--2dp">
+   <thead>
+      <tr>
+         <th>Id</th>
+         <th class="mdl-data-table__cell--non-numeric">Name</th>
+         <th>e-mail</th>
+         <th>Opções</th>
+      </tr>
+   </thead>
+<tbody>
 <?php 
 foreach($data['users'] as $user) {
 ?>
-   <tr>
-      <td><?php echo $user->id;?></td>
-      <td><?php echo $user->name;?></td>
-      <td><?php echo $user->email;?></td>
-      <td>
-         <a href='user/edit/<?php echo $user->id;?>'>Editar</a>
-         <a href='user/delete/<?php echo $user->id;?>'>Excluir</a>
-      </td>
-   </tr>
+   
+        <tr>
+          <td><?php echo $user->id;?></td>
+          <td><?php echo $user->name;?></td>
+         <td><?php echo $user->email;?></td>    
+          <td><a href='user/edit/<?php echo $user->id;?>'>Editar</a>
+         <a href='user/delete/<?php echo $user->id;?>'>Excluir</a></td>
+        </tr>
+
 <?php } ?>
+</tbody>
+</table>
+
